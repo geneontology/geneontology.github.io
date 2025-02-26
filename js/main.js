673d372297c870a8d22fd6b9aa6db28100336aee
Chris Mungall <cjm@berkeleybop.org>
2025-02-26 13:40:42 -0800

    Revert "Add functionome to homepage"

--- js/main.js
+++ js/main.js
@@ -38,14 +38,14 @@ $(function() {
 
     radio.change(function(e) {
 		choice = this.value;
-
+		
         console.log("receive input: ", e);
         console.log("form: ", form);
 
         if (choice === 'ontology') {
 			form.attr('action', 'http://amigo.geneontology.org/amigo/search/ontology');
 		} else if(choice == 'geneproduct') {
-			form.attr('action', 'http://amigo.geneontology.org/amigo/search/bioentity');
+			form.attr('action', 'http://amigo.geneontology.org/amigo/search/bioentity');			
         } else {
             form.attr('action', 'http://amigo.geneontology.org/amigo/medial_search');
         }
@@ -55,22 +55,22 @@ $(function() {
     // build from http://software.broadinstitute.org/gsea/msigdb/geneset_page.jsp
     list = [
 
-        { "title": "Genes up-regulated during formation of blood vessels/angiogenesis (source: MSigDB)",
-           "data": [ "APOH", "APP", "CND2", "COL3A1", "COL5A2", "CXCL6", "FGFR1", "FSTL1", "ITGAV", "JAG1", "JAG2", "KCNJ8", "LPL", "LRPAP1", "LUM", "MSX1", "NRP1", "OLR1", "PDGFA", "PF4", "PGLYRP1", "POSTN", "PRG2", "PTK2", "S100A4", "SERPINA5", "SLCO2A1", "SPP1", "STC1", "THBD", "TIMP1", "TNFRSF21", "VAV2", "VCAN", "VEGFA", "VTN" ]
+        { "title": "Genes up-regulated during formation of blood vessels/angiogenesis (source: MSigDB)", 
+           "data": [ "APOH", "APP", "CND2", "COL3A1", "COL5A2", "CXCL6", "FGFR1", "FSTL1", "ITGAV", "JAG1", "JAG2", "KCNJ8", "LPL", "LRPAP1", "LUM", "MSX1", "NRP1", "OLR1", "PDGFA", "PF4", "PGLYRP1", "POSTN", "PRG2", "PTK2", "S100A4", "SERPINA5", "SLCO2A1", "SPP1", "STC1", "THBD", "TIMP1", "TNFRSF21", "VAV2", "VCAN", "VEGFA", "VTN" ] 
         },
 
-        { "title": "Genes up-regulated by activation of Notch signaling (source: MSigDB)",
+        { "title": "Genes up-regulated by activation of Notch signaling (source: MSigDB)", 
            "data": ["APH1A","ARRB1","CCND1","CUL1","DLL1","DTX1","DTX2","DTX4","FBXW11","FZD1","FZD5","FZD7","HES1","HEYL","JAG1","KAT2A","LFNG","MAML2","NOTCH1","NOTCH2","NOTCH3","PPARD","PRKCA","PSEN2","PSENEN","RBX1","SAP30","SKP1","ST3GAL6","TCF7L2","WNT2","WNT5A"]
         },
 
-        { "title": "Genes up-regulated by activation of WNT signaling through accumulation of beta catenin (source: MSigDB)",
+        { "title": "Genes up-regulated by activation of WNT signaling through accumulation of beta catenin (source: MSigDB)", 
            "data": ["CTNNB1","ADAM17","AXIN1","AXIN2","CCND2","CSNK1E","CTNNB1","CUL1","DKK1","DKK4","DLL1","DVL2","FRAT1","FZD1","FZD8","GNAI1","HDAC11","HDAC2","HDAC5","HEY1","HEY2","JAG1","JAG2","KAT2A","LEF1","MAML1","MYC","NCOR2","NCSTN","NKD1","NOTCH1","NOTCH4","NUMB","PPARD","PSEN2","PTCH1","RBPJ","SKP2","TCF7","TP53","WNT1","WNT5B","WNT6"]
         },
 
-        { "title": "Genes up-regulated by activation of hedgehog signaling (source: MSigDB)",
+        { "title": "Genes up-regulated by activation of hedgehog signaling (source: MSigDB)", 
            "data": ["ACHE","AMOT","CDK5R1","CDK6","CELSR1","CNTFR","CRMP1","DPYSL2","ETS2","GLI1","GPR56","HEY1","HEY2","L1CAM","LDB1","MYH9","NF1","NKX6-1","NRCAM","NRP1","NRP2","OPHN1","PLG","PML","PTCH1","RASA1","RTN1","SCG2","SHH","SLIT1","THY1","TLE1","TLE3","UNC5C","VEGFA","VLDLR"]
         }
-
+        
     ]
 
     currentPos = Math.round(Math.random() * list.length)
